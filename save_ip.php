76667a7bf5dffcd322abc61ac3e5925529b8b8b1
John <johndmart16@gmail.com>
2023-07-06 21:34:17 -0500

    wigga

---
+++ save_ip.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_GET["ip"])) {
+  $ip = $_SERVER["REMOTE_ADDR"]; // Get the user's IP address
+  $referer = $_GET["ip"]; // Get the referring URL (optional)
+
+  // Compose the email message
+  $to = "johndmart16@gmail.com";
+  $subject = "Users Ip Adress";
+  $message = "IP Address: " . $ip . "\n";
+  $message .= "Referring URL: " . $referer . "\n";
+
+  // Send the email
+  $headers = "From: johndmart16@gmail.com" . "\r\n" .
+             "X-Mailer: PHP/" . phpversion();
+  mail($to, $subject, $message, $headers);
+}
+?>
